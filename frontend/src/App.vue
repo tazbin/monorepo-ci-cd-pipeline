95eadaad104df3fd063214296426a9779de28e5d
tazbin <mdtazbinur@gmail.com>
2025-04-02 23:20:55 +0600

    file change

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+                <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     This is a sample frontend application...
   </header>
